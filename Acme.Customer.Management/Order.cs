620a75f92da42e143cdf84bd46589ad63d9b966f
Haridas <haridasnykiel@gmail.com>
2020-04-01 12:58:33 +0100

    Added the order to customer and oreder to address relationships

--- Acme.Customer.Management/Order.cs
+++ Acme.Customer.Management/Order.cs
@@ -4,18 +4,20 @@ using System.Collections.Generic;
 namespace Acme.Customer.Management {
     public class Order {
 
-        public Order()
+        public Order() : this(0)
         {
             
         }
 
         public Order(int orderId) {
             OrderId = orderId;
+            OrderItems = new List<OrderItem>();
         }
 
         public int OrderId { get; set; }
         public DateTimeOffset? OrderDate { get; set; } 
-        public Address ShippingAddress { get; set; } // Composite relationship
+        public int CustomerId { get; set; } // Composite relationship
+        public int ShippingAddressId { get; set; } // Composite relationship
         public List<OrderItem> OrderItems { get; set; } // Composite relationships
         
         public bool Validate () {
